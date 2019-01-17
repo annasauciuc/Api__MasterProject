a1db897ac0bdb898e8f977a52394958fcef182c7
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-01-17 15:24:43 +0000

    getting the right id for the source image cats

--- starwars/src/components/CardList/CardList.js
+++ starwars/src/components/CardList/CardList.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import Card from './../Card/Card';
+import React from "react";
+import Card from "./../Card/Card";
 
 const CardList = ({ movies }) => {
-    return (
-        <div className="container">
-         <div className="row" > {
-             
-        movies.map((user, i) => {
-            return ( <
-                Card key = { i }
-                id = { movies[i].id }
-               
-                title = { movies[i].title }
-                release_date = { movies[i].release_date }
-                director = { movies[i].director }
-                opening_crawl = { movies[i].opening_crawl }
-                />
-            );
-            
-        })
-        
-    } </div></div>);
-}
+  return (
+    <div className="container">
+      <div className="row">
+        {" "}
+        {movies.map((movie, i) => {
+          return (
+            <Card
+              key={i}
+              id={i}
+              title={movie.title}
+              release_date={movie.release_date}
+              director={movie.director}
+              opening_crawl={movie.opening_crawl}
+            />
+          );
+        })}{" "}
+      </div>
+    </div>
+  );
+};
 export default CardList;
